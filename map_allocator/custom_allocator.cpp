6be5c7d5b3d6517f24e9e683478411c665a48ca8
A.Ivanyuk <iav@ppl33-35.net>
2021-12-20 14:01:12 +0200

    1

---
+++ map_allocator/custom_allocator.cpp
@@ -0,0 +1,35 @@
+﻿// custom_allocator.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <map>
+
+int fact(int n)
+{
+    if (n == 0)
+        return 0;
+    else
+        return n + fact(n - 1);
+
+}
+int main()
+{
+    std::map <int, int> mp = {
+                                {1,11},
+                                {2,22},
+                                {3,33},
+                                {4,44}
+                             };
+    int i=0;
+    //fill
+     for (int i =0;i<10;i++)
+         mp.insert(std::make_pair(i, fact(i)));
+         
+    //print
+    for (auto it = mp.begin(); it != mp.end(); it++)
+        std::cout << it->first << ' '<<it->second << std::endl;
+
+
+    std::cout << "Hello World!\n";
+}
+
---
+++ memory_allocations/memory_test.cpp
@@ -0,0 +1,58 @@
+﻿// memory_test.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <memory>
+
+struct AllocationMetrics
+{
+    uint32_t TotalAllocated = 0;
+    uint32_t TotalFreed = 0;
+    
+
+    uint32_t Currentusage() { return TotalAllocated - TotalFreed; }
+
+};
+
+static AllocationMetrics s_AllocationMetrics;
+
+void* operator new(size_t size)
+{
+    s_AllocationMetrics.TotalAllocated += size;
+    return malloc(size);
+}
+
+void operator delete(void* memory, size_t size)
+{
+    s_AllocationMetrics.TotalFreed += size;
+    free(memory);
+}
+struct Object
+{
+    int x, y, z;
+};
+
+static void PrintMemoryUsage()
+{
+    std::cout << "Memory usage: " << s_AllocationMetrics.Currentusage() << "bytes\n";
+}
+int main()
+{
+   // Object* obj = new Object;
+    try
+    {
+        {
+            std::unique_ptr<Object> obj = std::make_unique<Object>();
+            PrintMemoryUsage();
+        }
+        PrintMemoryUsage();
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr<< e.what() << std::endl;
+    }
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
